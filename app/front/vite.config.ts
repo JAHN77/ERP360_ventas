65d83c35c32bb111e68ec3f84bb96cc8f0b326cd
Juan Henriquez <juanandres072004@hotmail.com>
2025-12-17 08:43:48 -0500

    fix: remove single-spa config for vercel deploy

--- app/front/vite.config.ts
+++ app/front/vite.config.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import vitePluginSingleSpa from 'vite-plugin-single-spa';
+
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
@@ -12,15 +12,9 @@ export default defineConfig(({ mode }) => {
     server: {
       port: 4203,
       host: '0.0.0.0',
-      cors: true, // Importante para Single-SPA
     },
     plugins: [
       react(),
-      vitePluginSingleSpa({
-        type: 'mife', // microfrontend
-        serverPort: 4203,
-        spaEntryPoint: './index.tsx',
-      }),
     ],
     define: {
       'process.env.API_KEY': JSON.stringify(geminiApiKey),
@@ -32,17 +26,6 @@ export default defineConfig(({ mode }) => {
       }
     },
     build: {
-      rollupOptions: {
-        external: ['react', 'react-dom'], // React se comparte desde el root-config
-        output: {
-          format: 'systemjs',
-          entryFileNames: 'main.js',
-          globals: {
-            'react': 'React',
-            'react-dom': 'ReactDOM'
-          }
-        }
-      },
       chunkSizeWarningLimit: 1000
     }
   };
